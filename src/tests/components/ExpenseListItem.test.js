493a8b9c69e7d787762ef119ce944822d4b99894
Eric Barb <eric.barb@gmail.com>
2018-11-25 22:33:08 -0500

    Adding some formating

--- src/tests/components/ExpenseListItem.test.js
+++ src/tests/components/ExpenseListItem.test.js
@@ -4,7 +4,7 @@ import ExpenseListItem from '../../components/ExpenseListItem';
 import expenses from '../fixtures/expenses';
 
 test('should render ExpenseListItem with an expense', () => {
-    const expense = expenses[0];
-    const wrapper = shallow(<ExpenseListItem {...expense[0]} />);
+    //const expense = expenses[0];
+    const wrapper = shallow(<ExpenseListItem {...expenses[0]} />);
     expect(wrapper).toMatchSnapshot();
 });
