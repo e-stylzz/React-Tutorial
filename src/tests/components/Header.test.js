9c72cb201a720fc5645065d9434dfa27d2e127eb
Eric Barb <eric.barb@gmail.com>
2018-12-01 22:29:11 -0500

    Added some auth related stuff (login logout routing) and hooking up to state.

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 //import toJSON from 'enzyme-to-json';
-import Header from '../../components/Header';
+import { Header } from '../../components/Header';
 //import ReactShallowRenderer from 'react-test-renderer/shallow';
 
 test('should render header correctly', () => {
-    const wrapper = shallow(<Header />);
+    const wrapper = shallow(<Header startLogout={() => { }}/>);
     expect(wrapper).toMatchSnapshot();
     
     // expect(wrapper.find('h1').length).toBe(1);
@@ -15,4 +15,11 @@ test('should render header correctly', () => {
     // const renderer = new ReactShallowRenderer();
     // renderer.render(<Header />);
     // expect(renderer.getRenderOutput()).toMatchSnapshot();
+});
+
+test('should call startLogout on button click', () => {
+    const startLogout = jest.fn();
+    const wrapper = shallow(<Header startLogout={startLogout} />);
+    wrapper.find('button').simulate('click');
+    expect(startLogout).toHaveBeenCalled();
 });
